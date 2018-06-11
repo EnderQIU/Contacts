3d3f87e0fa684021402553a01560c6985b7e4c4e
enderqiu <a934560824@163.com>
2018-06-11 09:24:24 +0800

    added model classes

---
+++ Contacts/Contacts/Models/ContactsSet.h
@@ -0,0 +1,13 @@
+//
+//  ContactsSet.h
+//  Contacts
+//
+//  Created by enderqiu on 2018/6/10.
+//  Copyright © 2018年 enderqiu.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsSet : NSObject
+
+@end
